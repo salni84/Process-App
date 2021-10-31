c87b7f9b81c5408001b0a209909e9a50ae2f4817
Roberto Salerno <roberto.salerno@swisscom.com>
2021-10-31 18:41:16 +0100

    fix-json

--- server.js
+++ server.js
@@ -1,20 +1,13 @@
 const express = require('express');
+const path = require('path');
+
 const app = express();
-// If an incoming request uses
-// a protocol other than HTTPS,
-// redirect that request to the
-// same url but with HTTPS
-const forceSSL = function() {
-  return function (req, res, next) {
-    if (req.headers['x-forwarded-proto'] !== 'https') {
-      return res.redirect(
-        ['https://', req.get('Host'), req.url].join('')
-      );
-    }
-    next();
-  }
-}
-// Instruct the app
-// to use the forceSSL
-// middleware
-app.use(forceSSL());
+
+app.use(express.static(__dirname + '/dist/<FOLDER_NAME>'));
+
+app.get('/*', (req,res,next) => {
+  res.sendFile(path.join(__dirname + '/dist/dictionary/index.html'));
+});
+
+
+app.listen(process.env.PORT || 8000);
